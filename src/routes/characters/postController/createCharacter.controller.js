4cbda1f910baefc3e30cf660c3839c397794afe1
edarcode <edarcode@gmail.com>
2022-07-19 14:19:42 -0500

    advance(src/routes/characters/postController/createCharacter.controller.js)

--- src/routes/characters/postController/createCharacter.controller.js
+++ src/routes/characters/postController/createCharacter.controller.js
@@ -1,6 +1,11 @@
+const { createCharacter } = require("../../../utils/createCharacter");
+
 const createCharacterController = async (req, res, next) => {
 	try {
-		res.status(200).json({ msg: "successfully" });
+		const character = await createCharacter(req.body);
+		if (!character)
+			return res.status(200).json({ msg: "already exists", character });
+		res.status(201).json({ msg: "successfully", character });
 	} catch (error) {
 		next(error);
 	}
---
+++ src/utils/createCharacter.js
@@ -0,0 +1,18 @@
+const { Character } = require("../db");
+
+module.exports = {
+	createCharacter: async ({ name, age, weight, history, img, films }) => {
+		const [character, created] = await Character.findOrCreate({
+			where: { name },
+			defaults: {
+				age,
+				weight,
+				history,
+				img
+			}
+		});
+		if (!created) return null;
+		await character.setFilms(films);
+		return character;
+	}
+};
