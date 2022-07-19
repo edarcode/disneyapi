2b53d06306ba172758e47741206c5d35ba39b725
edarcode <edarcode@gmail.com>
2022-07-19 05:23:18 -0500

    feat(src/utils/encryptPassword.js) encriptando password

--- src/utils/register.js
+++ src/utils/register.js
@@ -1,11 +1,12 @@
 const { User } = require("../db");
+const { encryptPassword } = require("./encryptPassword");
 
 module.exports = {
 	register: async ({ email, password }) => {
 		const [user, created] = await User.findOrCreate({
 			where: { email },
 			defaults: {
-				password
+				password: await encryptPassword(password)
 			}
 		});
 		if (!created) return null;
