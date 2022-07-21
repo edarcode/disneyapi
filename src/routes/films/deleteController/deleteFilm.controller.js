1576866ab828ad491ea0108a510fb8f10bbcbf70
edarcode <edarcode@gmail.com>
2022-07-21 11:29:49 -0500

    feat(src/routes/films/deleteController/deleteFilm.controller.js) eliminar película/serie

--- src/routes/films/deleteController/deleteFilm.controller.js
+++ src/routes/films/deleteController/deleteFilm.controller.js
@@ -1,10 +1,15 @@
-// const deleteCharacterController = async (req, res, next) => {
-// 	try {
-// 		if (!isDestroy) return res.status(400).json({ msg: "err" });
-// 		res.status(200).json({ msg: "destroy successfully" });
-// 	} catch (error) {
-// 		next(error);
-// 	}
-// };
+const { destroyTuple } = require("../../../utils/destroyTuple");
+const { Film } = require("../../../db");
 
-// module.exports = deleteCharacterController;
+const deleteFilmController = async (req, res, next) => {
+	try {
+		const { id } = req.query;
+		const isDestroy = await destroyTuple({ id, model: Film });
+		if (!isDestroy) return res.status(400).json({ msg: "err" });
+		res.status(200).json({ msg: "destroy successfully" });
+	} catch (error) {
+		next(error);
+	}
+};
+
+module.exports = deleteFilmController;
--- src/routes/films/films.router.js
+++ src/routes/films/films.router.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const deleteFilmController = require("./deleteController/deleteFilm.controller");
 const getAllFilmsController = require("./getController/getAllFilms.controller");
 const getDetailFilmController = require("./getController/getDetailFilm.controller");
 const createFilmController = require("./postController/createFilm.controller");
@@ -7,5 +8,6 @@ const films = Router();
 films.route("/").post(createFilmController);
 films.route("/:id").get(getDetailFilmController);
 films.route("/").get(getAllFilmsController);
+films.route("/").delete(deleteFilmController);
 
 module.exports = films;
