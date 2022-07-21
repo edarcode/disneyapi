0aa03a2205c8b4e267636202f3fe1de0239220bb
edarcode <edarcode@gmail.com>
2022-07-21 08:49:28 -0500

    feat(src/utils/getAllCharacters.js) búsqueda de personajes con filtros

--- src/utils/getAllCharacters.js
+++ src/utils/getAllCharacters.js
@@ -1,12 +1,14 @@
 const { charactersPerPage } = require("../constants/perPage");
-const { Character } = require("../db");
+const { Character, Film, Op } = require("../db");
 
 module.exports = {
-	getAllCharacters: async ({ page = 0 }) => {
+	getAllCharacters: async ({ page = 0, name, age, weight, films }) => {
 		const { count, rows } = await Character.findAndCountAll({
+			where: where({ name, age, weight }),
 			offset: charactersPerPage * page,
 			limit: charactersPerPage,
-			attributes: ["id", "img", "name"]
+			attributes: ["id", "img", "name"],
+			include: include({ films })
 		});
 		const data = {
 			pageCount: Math.ceil(count / charactersPerPage),
@@ -18,3 +20,39 @@ module.exports = {
 		return data;
 	}
 };
+
+function where({ name, age, weight }) {
+	const result = {};
+	if (name) result.name = { [Op.iLike]: `%${name}%` };
+	if (age) {
+		if (age === "null") {
+			result.age = null;
+		} else {
+			result.age = age;
+		}
+	}
+	if (weight) {
+		if (weight === "null") {
+			result.weight = null;
+		} else {
+			result.weight = weight;
+		}
+	}
+	return result;
+}
+function include({ films }) {
+	let result = [];
+	if (films) {
+		const filmsJs = JSON.parse(films);
+		result = [
+			{
+				model: Film,
+				as: "films",
+				where: { id: { [Op.and]: filmsJs } },
+				attributes: [],
+				through: { attributes: [] }
+			}
+		];
+	}
+	return result;
+}
