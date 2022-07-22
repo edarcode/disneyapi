cf538c8cab309ebb61ec6ce6c41b1bc4f5cf78fd
edarcode <edarcode@gmail.com>
2022-07-22 01:22:27 -0500

    refactor(src/routes/genres/postController/createGenre.controller.js)

--- src/routes/genres/postController/createGenre.controller.js
+++ src/routes/genres/postController/createGenre.controller.js
@@ -3,6 +3,6 @@ const { createGenre } = require("../../../utils/createGenre");
 const createGenreController = async (req, res) => {
 	const genre = await createGenre(req.body);
 	if (!genre) return res.status(200).json({ msg: "already exits" });
-	res.status(201).json({ msg: "successfully", genre });
+	res.status(201).json({ msg: "successfully" });
 };
 module.exports = createGenreController;
