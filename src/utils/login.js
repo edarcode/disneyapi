0fce95a6316211184156e05aa3a7e334659eb600
edarcode <edarcode@gmail.com>
2022-07-23 04:33:09 -0500

    refactor(src/utils/login.js)

--- src/utils/login.js
+++ src/utils/login.js
@@ -3,9 +3,11 @@ const { comparePassword } = require("./comparePassword");
 
 module.exports = {
 	login: async ({ email, password }) => {
+		if (!email || !password) return null;
 		const user = await User.findOne({
 			where: { email }
 		});
+		if (!user) return null;
 		const isAuth = await comparePassword(password, user.password);
 		if (!isAuth) return null;
 		return user;
